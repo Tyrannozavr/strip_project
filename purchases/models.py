e439a6241370d6e9395ab799f53d22b954fe5f45
dmiv <dmitriy40647274@gmail.com>
2022-11-21 16:28:23 +0300

    add fields percent_off and name in Discount

--- purchases/models.py
+++ purchases/models.py
@@ -16,6 +16,8 @@ class Product(models.Model):
 
 class Discount(models.Model):
     id_coupon = models.CharField(max_length=256)
+    percent_off = models.CharField(max_length=256)
+    name = models.CharField('Name', max_length=256, null=True, blank=True)
 
     def simple_create(percent_off, duration):
         """simple creating discount without visiting site percent_off can't be 100% and duration must be once,
@@ -25,7 +27,10 @@ class Discount(models.Model):
             raise Exception('duraton must be once, repeating or forever')
         stripe.api_key = settings.STRIPE_SECRET_KEY
         coupon = stripe.Coupon.create(percent_off=percent_off, duration=duration)
-        Discount.objects.create(id_coupon=coupon.id)
+        Discount.objects.create(id_coupon=coupon.id, percent_off=coupon.percent_off, name=coupon.name)
+
+    def __str__(self):
+        return self.name if not self.name is None else 'None'
 
 
 class Order(models.Model):
